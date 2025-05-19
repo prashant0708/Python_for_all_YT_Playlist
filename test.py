6e2a60e8bf752a68cca56d3536d1fb1c33bfdbf0
prashant kumar singh <prashant.singh2012p@gmail.com>
2025-05-20 00:51:35 +0530

    print_function done

---
+++ test.py
@@ -0,0 +1,9 @@
+name = "Prashant Singh"
+description = "I am Youtuber and I create educational video"
+address = "Red Fort Delhi"
+nationality = "Indian"
+
+print(name)
+print(description)
+print(address)
+print(nationality)
