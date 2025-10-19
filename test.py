61b34d6f6bc3d1b377d4e3e3259a458ad6a416f7
Prashant <prashantsingh.aiengineer@gmail.com>
2025-10-19 23:31:49 +0530

    control flow

--- test.py
+++ test.py
@@ -1,9 +1,16 @@
-name = "Prashant Singh"
-description = "I am Youtuber and I create educational video"
-address = "Red Fort Delhi"
-nationality = "Indian"
 
-print(name)
-print(description)
-print(address)
-print(nationality)
+## python interview question find the factorical of given number
+
+number  = int(input("Enter your number = "))
+factorial = 1
+if number == 0:
+    print("factorial of 0 is 1")
+    exit()
+elif number ==1:
+    print("factorial of 1 is 1")
+    exit()
+else:
+    for i in range(1,number+1): 
+     factorial = factorial * i
+
+print(f"factorial of {number} is {factorial}")
